ddb0af95f3f0575fea2440570fbefde9fd49a3a5
Maxim Tolstokory <maxim.code@outlook.com>
2021-07-29 23:23:10 -0800

    feat: load user info via api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,37 @@
+import { api } from '../utils/Api.js';
+import { useState, useEffect } from 'react';
+
 export default function Main(props) {
+
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('http://');
+
+  useEffect(() => {
+    api.getUserInfo()
+    .then(info => {
+      setUserName(info.name);
+      setUserDescription(info.about);
+      setUserAvatar(info.avatar);
+    })
+    .catch(err => {console.log(err)});
+  });
 
   return (
     <main className="content body__element">
       <section className="profile body__element">
         <div className="profile__pic-info-container">
           <div className="profile__pic-container">
-            <img src="http://" alt="фото аккаунта" className="profile__pic" />
+            <img src={userAvatar} alt="фото аккаунта" className="profile__pic" />
             <button className="profile__avatar-edit-button transparent transparent_amount_much-more" onClick={props.onEditAvatar}></button>
           </div>
           <div className="profile__info">
             <div className="profile__name-edit-wrap">
-              <h1 className="profile__name"></h1>
+              <h1 className="profile__name">{userName}</h1>
               <button className="profile__edit-button transparent transparent_amount_more"
                       type="button" aria-label="Кнопка редактирования профиля" onClick={props.onEditProfile}></button>
             </div>
-            <p className="profile__description"></p>
+            <p className="profile__description">{userDescription}</p>
           </div>
         </div>
         <button className="profile__add-button transparent transparent_amount_more"
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,6 +8,11 @@ const popupConfig = {
   profileEditButtonSelector: '.profile__edit-button',
   profileEditPopupAndFormName: 'edit-profile',
   cardConfirmDeletePopupAndFormName: 'confirm-delete',
-}
+};
 
-export {popupConfig};
+const apiConfig = {
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-25/',
+  authorization: 'd8d84bac-32d7-42f9-a622-bbe14f1aa9f5'
+};
+
+export {popupConfig, apiConfig};
